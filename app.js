db26a64700c15fa0fe6f04222bb94c95d678616f
Baa1 <50644318+Baa1@users.noreply.github.com>
2021-10-12 01:33:30 +0700

    queries added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/auth', authRouter)
-app.use('/books', booksRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/books', booksRouter)
 
 module.exports = app
--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -2,17 +2,22 @@ const client = require('../db')
 
 exports.create = async (req, res) => {
     try {
-        
+        let { title, date, author, description, image } = req.body
+        let params = [title, date, author, description, image]
+        console.log(params)
+        let result = (await client.query('INSERT INTO books (title, date, author, description, image) VALUES ($1, $2, $3, $4, $5) RETURNING *', params))
+        return res.send({ book: result.rows[0] })
     } catch (error) {
-        
+        return res.status(500).send({ message: error.message })
     }
 }
 
 exports.getAll = async (req, res) => {
     try {
-        
+        let books = (await client.query('SELECT * FROM books')).rows
+        return res.send(books)
     } catch (error) {
-        
+        return res.status(500).send({ message: error.message })
     }
 }
 
