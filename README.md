d2bc4a5f54107dce7dedc4eaa5bb1a6beb4c54f5
Baa1 <50644318+Baa1@users.noreply.github.com>
2021-10-13 08:08:48 +0700

    migration added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # solution-factory-test
  
+# setup project
+npm install
+
+# creating database tables from migration
+npm run migrate up
+
+# compiles and hot-reloads for development
+npm run dev
+
+# run app in debug mode (without hot-reloads)
+set DEBUG=solution-factory-test:* & npm start
---
+++ migrations/sqls/20211013005403-test-up.sql
@@ -0,0 +1,83 @@
+/* Replace with your SQL commands */
+-- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
+(
+    id serial NOT NULL,
+    login character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_login_key UNIQUE (login)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
+
+-- Table: public.authors
+
+-- DROP TABLE public.authors;
+
+CREATE TABLE public.authors
+(
+    id serial NOT NULL,
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    surname character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    patronymic character varying(50) COLLATE pg_catalog."default",
+    CONSTRAINT authors_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.authors
+    OWNER to postgres;
+
+-- Table: public.images
+
+-- DROP TABLE public.images;
+
+CREATE TABLE public.images
+(
+    id serial NOT NULL,
+    filename character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    ext character varying(5) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT images_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.images
+    OWNER to postgres;
+
+-- Table: public.books
+
+-- DROP TABLE public.books;
+
+CREATE TABLE public.books
+(
+    id serial NOT NULL,
+    title character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    date date,
+    author integer,
+    description character varying(500) COLLATE pg_catalog."default",
+    image integer,
+    CONSTRAINT books_pkey PRIMARY KEY (id),
+    CONSTRAINT books_author_fkey FOREIGN KEY (author)
+        REFERENCES public.authors (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT books_image_fkey FOREIGN KEY (image)
+        REFERENCES public.images (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.books
+    OWNER to postgres;
