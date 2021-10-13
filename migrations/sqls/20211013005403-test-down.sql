7ab6d99540d82cb179c4e8164e7394766b24cd47
Baa1 <50644318+Baa1@users.noreply.github.com>
2021-10-13 20:46:31 +0700

    Update 20211013005403-test-down.sql

--- migrations/sqls/20211013005403-test-down.sql
+++ migrations/sqls/20211013005403-test-down.sql
@@ -1,1 +1,16 @@
 /* Replace with your SQL commands */
+-- Table: public.books
+
+DROP TABLE public.books;
+
+-- Table: public.images
+
+DROP TABLE public.images;
+
+-- Table: public.authors
+
+DROP TABLE public.authors;
+
+-- Table: public.users
+
+DROP TABLE public.users;
