87d639f50ff898012b54f50ca73a78d6895f0be3
Baa1 <50644318+Baa1@users.noreply.github.com>
2021-10-13 08:37:21 +0700

    Update books.routes.js

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -7,6 +7,8 @@ const { check } = require('express-validator')
 router.post(
     '/', 
     check('title', 'Поле title обязательно для заполнения').notEmpty(),
+    check('title', 'Поле title не должно превышать 200 символов').isLength({ max: 200 }),
+    check('description', 'Поле description не должно превышать 500 символов').isLength({ max: 500 }),
     [authJwt.verifyToken], 
     controller.create
 )
@@ -16,6 +18,8 @@ router.get('/', [authJwt.verifyToken], controller.getAll)
 router.put(
     '/', 
     check('title', 'Поле title обязательно для заполнения').notEmpty(), 
+    check('title', 'Поле title не должно превышать 200 символов').isLength({ max: 200 }),
+    check('description', 'Поле description не должно превышать 500 символов').isLength({ max: 500 }),
     [authJwt.verifyToken], 
     controller.update
 )
