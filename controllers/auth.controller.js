4cb2fd56af0739122cb8d831486007f2b419aee3
Baa1 <50644318+Baa1@users.noreply.github.com>
2021-10-14 08:30:44 +0700

    signIn changed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,5 @@
-const config = require('../config/auth.config')
-const client = require('../db')
+
 const { validationResult } = require('express-validator')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
 const { authService } = require('../services')
 
 exports.signUp = async (req, res) => {
@@ -21,37 +18,20 @@ exports.signUp = async (req, res) => {
     }
 }
 
-exports.signIn = (req, res) => {
+exports.signIn = async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errors: errors.array() 
         })
     }
-    client.query('SELECT id, login, password FROM users WHERE login = $1', [req.body.login])
-    .then(result => {
-        if (result.rows.length === 0) return res.status(404).send({ message: 'User Not found.' })
-        let user = result.rows[0]
-        const passwordIsValid = bcrypt.compareSync(
-			req.body.password,
-			user.password
-		)
-        if (!passwordIsValid) {
-			return res.status(401).send({
-				accessToken: null,
-				message: 'Invalid Password!'
-			})
-		}
-        const token = jwt.sign({ id: user.id }, config.secret, {
-			expiresIn: config.jwtExpiration
-		})
-        return res.send({
-            id: user.id,
-            login: user.login,
-            accessToken: token,
-        })
-    })
-    .catch(error => {
+    try {
+        let { login, password } = req.body
+        let user = await authService.signIn(login, password)
+        if (!user) return res.status(404).send({ message: 'User Not found.' })
+        if (!user.accessToken) return res.status(401).send({ accessToken: null,	message: 'Invalid Password!' })
+        res.send(user)
+    } catch (error) {
         return res.status(500).send({ message: error.message })
-    })
+    }
 }
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,5 +1,7 @@
 const pool = require('../db')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const config = require('../config/auth.config')
 
 class AuthService {
     async signUp(login, password) {
@@ -7,17 +9,34 @@ class AuthService {
         try {
             await client.query('BEGIN')
             let result = await client.query('INSERT INTO users (login, password) VALUES ($1, $2) RETURNING id', [login, bcrypt.hashSync(password, 8)])
-            if (result.rows && result.rows.length === 1) return result.rows[0].id
-            await client.query('COMMIT')
+            if (result.rows && result.rows.length === 1) {
+                await client.query('COMMIT')
+                return result.rows[0].id
+            }
         } catch (error) {
             await client.query('ROLLBACK')
             throw error
         } finally {
-            client.release()
+            await client.release()
         }
     }
-    signIn(login, password) {
-
+    async signIn(login, password) {
+        let client = await pool.connect()
+        try {
+            let result = await client.query('SELECT id, login, password FROM users WHERE login = $1', [login])
+            if (result.rows && result.rows.length === 0) return null
+            let user = result.rows[0]
+            const passwordIsValid = bcrypt.compareSync(password, user.password)
+            if (!passwordIsValid) return { accessToken: null }
+            const token = jwt.sign({ id: user.id }, config.secret, {
+                expiresIn: config.jwtExpiration
+            })
+            return { id: user.id, login: user.login, accessToken: token }
+        } catch (error) {
+            throw error
+        } finally {
+            client.release()
+        }
     }
 }
 
